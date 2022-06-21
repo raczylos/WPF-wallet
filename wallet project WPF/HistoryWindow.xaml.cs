f60a49a62a75b464b216a317d710dff74b03291d
Sebastian <sebastian.raczylo@gmail.com>
2022-06-21 04:08:18 +0200

    half of filters done

--- wallet project WPF/HistoryWindow.xaml.cs
+++ wallet project WPF/HistoryWindow.xaml.cs
@@ -20,9 +20,104 @@ namespace wallet_project_WPF
     /// </summary>
     public partial class HistoryWindow : Window
     {
+        Transation[] transations = new Transation[]
+        {
+            new Transation("test1", 100, "kredyt"),
+            new Transation("test2", 200, "produkty spozywcze")
+        };
+
         public HistoryWindow()
         {
             InitializeComponent();
+
+            transactionList.ItemsSource = transations;
+            //categoryComboBox.ItemsSource = new string[] { "Title", "Price", "Category" };
+            //categoryComboBox.ItemsSource = typeof(Transation).GetProperties().Select((k) => k.Name);
+            List<String> list = new List<String>();
+            list.Add("");
+            foreach (Transation transation in transations)
+            {
+
+                list.Add(transation.category);
+                
+            }
+            categoryComboBox.ItemsSource = list;
+
+            
+
+        }
+
+       
+        
+
+        private bool MyFilter(object obj)
+        {
+            //MessageBox.Show("robie filtr");
+            var filterObj = obj as Transation;
+            var text = categoryComboBox.SelectedItem.ToString();
+
+
+            var selectedCategory = categoryComboBox.SelectedItem.ToString();
+            var selectedPriceFrom = priceFrom.Text;
+            var selectedPriceTo = priceTo.Text;
+
+            //return filterObj.category.Contains(categoryComboBox.Text);
+
+            if (selectedCategory == "" && selectedPriceFrom == "" && selectedPriceTo == "")
+            {
+                return true;
+            }
+
+            if(selectedPriceFrom == "")
+            {
+                selectedPriceFrom = "0";
+            }
+
+            if (selectedPriceTo == "")
+            {
+                selectedPriceTo = "2147483647";
+            }
+
+
+            if (filterObj.category.Contains(selectedCategory) && (filterObj.price > Convert.ToInt32(selectedPriceFrom)) && (filterObj.price < Convert.ToInt32(selectedPriceTo)))
+            {
+                return true;
+            }
+            
+
+
+            return false;
+        }
+
+        private bool TransactionFilter(object item)
+        {
+
+            return true;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CategoriesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            transactionList.Items.Filter = MyFilter;
+            //categoryComboBox.ItemsSource
+
+
+
+        }
+
+        private void priceFrom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            transactionList.Items.Filter = MyFilter;
+        }
+
+        private void priceTo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            transactionList.Items.Filter = MyFilter;
         }
     }
 }
---
+++ wallet project WPF/Transation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wallet_project_WPF
+{
+    public class Transation
+    {
+        public string title { get; set; }
+        public int price { get; set; }
+
+        public string category { get; set; }
+
+        public Transation(string title, int price, string category)
+        {
+            this.title = title;
+            this.price = price;
+            this.category = category;
+        }
+
+       
+    }
+}
