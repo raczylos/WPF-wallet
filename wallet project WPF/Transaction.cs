f8113ddd55f028c2e48b159239eb49855e9eec03
szymonszymonszymonv <szymon.prokopiuk99@gmail.com>
2022-06-21 03:58:15 +0200

    cos tam cos tam

--- wallet project WPF/Transaction.cs
+++ wallet project WPF/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace wallet_project_WPF {
     public class Transaction {
         public int TransactionId { get; set; }
         public double? MoneyAmount { get; set; }
-        public bool? isIncoming { get; set; } // incoming: ++money | outgoing: --money
+        public bool? isIncoming { get; set; } // incoming: ++money 
+        [NotMapped]
+        public bool? isOutgoing { get { return !isIncoming; } } // outgoing: --money
         public bool? isCycle { get; set; } // transaction repeats every month
         public DateTime? date { get; set; } // day of the month
         public virtual Category? Category { get; set; }
